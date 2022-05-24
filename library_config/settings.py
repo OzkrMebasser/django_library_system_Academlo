980d13cd7237cca826bf0481c601429c0a5b7ee6
OzKr Mebasser <82064221+OzkrMebasser@users.noreply.github.com>
2022-05-24 00:33:40 -0500

    Procfile Heroku deploy

--- library_config/settings.py
+++ library_config/settings.py
@@ -134,3 +134,6 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import django_heroku
+django_heroku.settings(locals())
